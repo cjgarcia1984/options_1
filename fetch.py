d013eff8614814e53b10a8f1c17af874599a6696
Chris <cjgarcia1984@gmail.com>
2025-03-10 16:55:04 -0700

    fetch updates

--- fetch.py
+++ fetch.py
@@ -1,8 +1,10 @@
-from src.historical import HistoricalDataHandler
+from src.historical.historical import HistoricalDataHandler
+import logging
+import datetime
 
 if __name__ == "__main__":
-    config_path = "/home/chris/options_1/config.yaml"  # Ensure the path is correct
-
+    config_path = "/home/chris/options_1/configs/fetch_config.yaml"  # Ensure the path is correct
+    print(f"Starting fetch at time: {datetime.datetime.now()}")
     data_handler = HistoricalDataHandler(config_path=config_path)
     data_handler.fetch_and_store_options_data()
     data_handler.export_to_csv()
